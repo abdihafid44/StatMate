ee4b48f0f6155468dc7b83078603402b7cdd5a59
abdihafid44 <xaafid4476@gmail.com>
2026-05-24 17:27:36 +0300

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -30,7 +30,7 @@ export default async function handler(req, res) {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify({
-        model: 'llama3-8b-8192', // Blazing fast, free model
+        model: 'llama-3.1-8b-instant', // Blazing fast, free model
         messages: formattedMessages
       })
     });
